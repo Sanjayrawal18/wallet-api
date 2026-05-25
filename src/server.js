131de69004cd8ab62baf774caedd9b06e7fb7891
Sanjayrawal18 <rawalsanjay204@gmail.com>
2026-05-25 11:57:11 +0530

    minor additions

--- src/server.js
+++ src/server.js
@@ -5,10 +5,13 @@ import { initDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
 
 import transactionsRoute from "./routes/transactionsRoute.js";
+import job from "./config/cron.js";
 
 dotenv.config();
 
 const app = express();
+
+if (process.env.NODE_ENV === 'production') job.start();
 
 // middleware
 const allowedOrigins = ['http://localhost:8081', 'exp://10.131.69.200:8081'];
@@ -25,6 +28,10 @@ app.use(rateLimiter);
 app.use(express.json());
 
 const PORT = process.env.PORT || 5001;
+
+app.get("api/health", (req, res) => {
+  res.status(200).json({ status: "ok"})
+})
 
 app.use("/api/transactions", transactionsRoute);
 
